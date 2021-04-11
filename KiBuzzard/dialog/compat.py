0abe176c09a6f2ce37e4b253e4052df56a5c488c
Greg Davill <greg.davill@gmail.com>
2021-04-11 19:21:10 +0930

    Add init to dialogShim

--- KiBuzzard/dialog/compat.py
+++ KiBuzzard/dialog/compat.py
@@ -3,6 +3,11 @@ import wx
 
 class DialogShim(wx.Dialog):
     def __init__(self, parent, **kwargs):
+        try:
+            wx.StockGDI._initStockObjects()
+        except:
+            pass
+        
         wx.Dialog.__init__(self, parent, **kwargs)
 
     def SetSizeHints(self, a, b, c=None):
