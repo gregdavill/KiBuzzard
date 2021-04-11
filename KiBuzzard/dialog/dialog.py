79c9400246493795b2ba198ea8dde9ef9061b9b0
Greg Davill <greg.davill@gmail.com>
2021-04-11 19:40:04 +0930

    Pass exception to popup dialog box

--- KiBuzzard/dialog/dialog.py
+++ KiBuzzard/dialog/dialog.py
@@ -100,8 +100,14 @@ class Dialog(dialog_text_base.DIALOG_TEXT_BASE):
         self.buzzard.rightCap = styles[self.m_JustifyChoice.GetStringSelection()]
 
 
-        self.polys = self.buzzard.generate(self.m_MultiLineText.GetValue())
-        
+        try:
+
+            self.polys = self.buzzard.generate(self.m_MultiLineText.GetValue())
+        except Exception as e:
+            wx.LogError(str(e))
+            #print(e)
+            # Todo display error messages
+
         self.RePaint()
 
     def RePaint(self, e=None):
